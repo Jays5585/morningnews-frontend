4507191c94d7d0d07dd86357bdae1ffc2fe1352b
Jays5585 <147637269+Jays5585@users.noreply.github.com>
2024-01-23 12:55:23 +0100

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -13,7 +13,7 @@ function Home() {
   const [topArticle, setTopArticle] = useState({});
 
   useEffect(() => {
-    fetch('http://Backend-morningnews.vercel.app/articles')
+    fetch('http://backend-morningnews-c9or.vercel.app/articles')
       .then(response => response.json())
       .then(data => {
         setTopArticle(data.articles[0]);
