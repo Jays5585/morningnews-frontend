e91ad99d7cdc33026967650b0cc4b616bbd8d930
Jays5585 <147637269+Jays5585@users.noreply.github.com>
2024-01-23 09:41:26 +0100

    Update Article.js

--- components/Article.js
+++ components/Article.js
@@ -15,7 +15,7 @@ function Article(props) {
       return;
     }
 
-    fetch(`http://Backend-morningnews.vercel/users/canBookmark/${user.token}`)
+    fetch(`http://Backend-morningnews.vercel.app/users/canBookmark/${user.token}`)
       .then(response => response.json())
       .then(data => {
         if (data.result && data.canBookmark) {
