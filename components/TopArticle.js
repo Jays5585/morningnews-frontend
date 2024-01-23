5f694e73d1d6cfd04c3e3507b8bd2fdb0ad94b3d
Jays5585 <147637269+Jays5585@users.noreply.github.com>
2024-01-23 17:49:14 +0100

    Update TopArticle.js

--- components/TopArticle.js
+++ components/TopArticle.js
@@ -13,7 +13,7 @@ function TopArticle(props) {
       return;
     }
 
-    fetch(`http://backend-morningnews-c9or.vercel.app/users/canBookmark/${user.token}`)
+    fetch(`https://backend-morningnews-c9or.vercel.app/users/canBookmark/${user.token}`)
       .then(response => response.json())
       .then(data => {
         if (data.result && data.canBookmark) {
