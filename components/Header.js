a08020305ada9dee64f988a632403891104895b5
Jays5585 <147637269+Jays5585@users.noreply.github.com>
2024-01-23 17:48:40 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -26,7 +26,7 @@ function Header() {
   }, []);
 
   const handleRegister = () => {
-    fetch('http://backend-morningnews-c9or.vercel.app/users/signup', {
+    fetch('https://backend-morningnews-c9or.vercel.app/users/signup', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username: signUpUsername, password: signUpPassword }),
